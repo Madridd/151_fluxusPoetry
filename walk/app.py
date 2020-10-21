eab8753015d9516f7e6ead8ec3df1a02e6110a51
Ivan Madrid <ivanmadrid@Ivans-MBP.zyxel.com>
2020-10-21 11:52:47 -0500

    addedThirdPage

--- walk/app.py
+++ walk/app.py
@@ -30,3 +30,17 @@ def edm():
     text = [t for t in soup.find_all(text=True) if t.parent.name in whitelist]
     print (text)
     return render_template("pageTwo.html", text = text )
+
+@app.route('/pageThree')
+def hiphop():
+    headers = {'user-agent': 'Mozilla/5.0'}
+    page3 = requests.get("https://en.wikipedia.org/wiki/Hip_hop_music", headers=headers)
+    soup3 = BeautifulSoup(page3.content, 'html.parser')
+
+    whitelist3 = [
+    'p'
+    ]
+
+    text = [t for t in soup3.find_all(text=True) if t.parent.name in whitelist3]
+    print (text)
+    return render_template("pageThree.html", text = text )
